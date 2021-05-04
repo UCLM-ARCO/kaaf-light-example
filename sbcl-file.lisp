af9caa4c4bd29fc818b6f70050c19e915c142a02
Ana Rubio Ruiz <Ana.Rubio@uclm.es>
2021-05-04 03:12:11 +0200

    rm ice config and sbcl file

--- sbcl-file.lisp
+++
@@ -1,5 +0,0 @@
-(load "/usr/share/scone/scone-start.lisp")
-(load "scone-knowledge.d/01-general.lisp")
-(load "scone-knowledge.d/02-statuses.lisp")
-(load "scone-knowledge.d/03-events.lisp")
-(load "scone-knowledge.d/04-rules.lisp")
